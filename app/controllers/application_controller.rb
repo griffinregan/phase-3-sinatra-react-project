642ace2de8ca5902739984a4b7eaa0190e7a59b7
griffinregan <104036283+griffinregan@users.noreply.github.com>
2022-08-07 15:24:51 -0400

    updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,34 +2,56 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-
-
+  
+  
   get "/" do 
     {message: "Good luck with your project!" }.to_json
   end
-
+  
   get '/shows' do
-  Show.all.to_json
+    Show.all.to_json
+  end
+  
+  get '/users' do
+    User.all.to_json
   end
 
   get '/shows/:id' do
-    shows = Show.find(params[:id])
+    show = Show.find(params[:id])
     show.to_json
+    end
+
+    get '/users/:id' do
+      user = User.find(params[:id])
+      user.to_json
+      end
+
+  #Create show and user
+
+  post '/shows' do 
+    post_show = Show.create(params)
+    post_show.to_json
   end
 
-  post '/shows' do
-    show = Show.create(params)
-    show.to_json
+  post '/user' do 
+    post_user = User.create(params)
+    post_user.to_json
   end
 
-  post '/users' do
-   user = User.create(params)
-   user.to_json
-  end
+#Delete show
+delete '/shows/:id' do
+  delete_show = Show.find(params[:id])
+  delete_show.destroy
+  delete_show.to_json
+end
 
-  get '/users' do
-    user = user.all
-    user.to_json
-  end
+#Update show
+patch '/shows/:id' do
+  patch_show = Show.find(params[:id])
+  patch_show.update(params)
+  patch_show.to_json
+end
 
 end
+
+
